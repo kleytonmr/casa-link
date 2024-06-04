c7b6c18d7cc20e6119e59708b021195aca3f76a9
Kleytonmr <kleytonmatosramos@gmail.com>
2024-06-03 22:28:04 -0300

    change site to adriel and layla

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ function scrollToPixSection() {
 
 function copyPixKey() {
     var textArea = document.createElement("textarea");
-    textArea.value = "00020126360014BR.GOV.BCB.PIX0114+55279962140975204000053039865802BR5919KLEYTON MATOS RAMOS6005Serra62110507Casorio6304346A";
+    textArea.value = "00020126360014BR.GOV.BCB.PIX0114+55279999744565204000053039865802BR5915Adriel Henrique6005Serra62130509casamento6304616B"
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand("copy");
     document.body.removeChild(textArea);
     alert("Chave Pix copiada para a área de transferência!");
 }
-
